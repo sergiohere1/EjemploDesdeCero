8163829b28a93c20f899c833935f11675ae2b942
sergiohere1 <sergio11garcia@hotmail.com>
2018-01-09 09:34:19 +0100

    Día 2 - Full Movement

---
+++ Assets/Scripts/MoveAgain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAgain : MonoBehaviour {
+    public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+	}
+}
